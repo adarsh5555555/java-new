1c9e6c72bfa89ae8803f1ae3b0a0ee8c74f1277c
Adarsh <adarsh80810@gmail.com>
2025-05-27 16:51:05 +0530

    add this file 5

---
+++ src/file5.java
@@ -0,0 +1,18 @@
+public class file5 {
+    int noOfPersons;
+
+    @Override
+    public String toString() {
+        return "file5{" +
+                "noOfPersons=" + noOfPersons +
+                '}';
+    }
+
+    public int getNoOfPersons() {
+        return noOfPersons;
+    }
+
+    public void setNoOfPersons(int noOfPersons) {
+        this.noOfPersons = noOfPersons;
+    }
+}
