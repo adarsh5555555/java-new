8e64c5dfad65c9e4571cb5afc46068ee99669422
Adarsh <adarsh80810@gmail.com>
2025-05-27 20:57:35 +0530

    add and modify file 6 to codebase

--- src/file6.java
+++ src/file6.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class file6 {
     public static void main(String[] args) {
-        System.out.println("heyy adarsh");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number you want to check if palindrome or not :");
+        int num  = input.nextInt();
+        int numcopy = num;
+        int reverse = 0;
+        while(num>0){
+            num = num%10;
+            reverse = reverse*10+num;
+        }
+        if(reverse ==numcopy){
+            System.out.println("yess the number is palindrome");
+        }else{
+            System.out.println("Noo,the number is not palindrome");
+        }
     }
 }
