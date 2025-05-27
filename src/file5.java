a056ee211b512f18e7514368ac8a11061117e88e
Adarsh <adarsh80810@gmail.com>
2025-05-27 17:02:54 +0530

    modified file5 in main branch

--- src/file5.java
+++ src/file5.java
@@ -15,4 +15,11 @@ public class file5 {
     public void setNoOfPersons(int noOfPersons) {
         this.noOfPersons = noOfPersons;
     }
+    public void manypersons(){
+        if(this.noOfPersons>=5){
+            System.out.println("yess there are many persons");
+        }else{
+            System.out.println("no there are less persons");
+        }
+    }
 }
